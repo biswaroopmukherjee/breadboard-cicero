568900d3d4f521334218c55d724d30242308a95c
Biswaroop Mukherjee <roop@mit.edu>
2018-12-19 00:27:43 -0500

    V0.1: Watchfolders, snippet folders
    With a functional UI. Ready to replace the snippet server.

--- Breadboard/BreadboardMainForm.Designer.cs
+++ Breadboard/BreadboardMainForm.Designer.cs
@@ -28,42 +28,183 @@ namespace Breadboard
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreadboardMainForm));
             this.button1 = new System.Windows.Forms.Button();
-            this.MainLabel = new System.Windows.Forms.Label();
+            this.rlgFolderButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(217, 156);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(11, 79);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 28);
+            this.button1.Size = new System.Drawing.Size(69, 60);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Read Runlog";
+            this.button1.Text = "Manually Read Runlogs";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // MainLabel
+            // rlgFolderButton
             // 
-            this.MainLabel.AutoSize = true;
-            this.MainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MainLabel.Location = new System.Drawing.Point(202, 19);
-            this.MainLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.MainLabel.Name = "MainLabel";
-            this.MainLabel.Size = new System.Drawing.Size(125, 26);
-            this.MainLabel.TabIndex = 3;
-            this.MainLabel.Text = "Breadboard";
+            this.rlgFolderButton.Location = new System.Drawing.Point(234, 3);
+            this.rlgFolderButton.Name = "rlgFolderButton";
+            this.rlgFolderButton.Size = new System.Drawing.Size(78, 21);
+            this.rlgFolderButton.TabIndex = 4;
+            this.rlgFolderButton.Text = "Select";
+            this.rlgFolderButton.UseVisualStyleBackColor = true;
+            this.rlgFolderButton.Click += new System.EventHandler(this.rlgfolder_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 21);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.snippetfolder_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.07531F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.92469F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+            this.tableLayoutPanel1.Controls.Add(this.rlgFolderButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(325, 61);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Snippet Folder";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "RunLog Folder";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 61);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save Settings";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(343, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 58);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label4.Location = new System.Drawing.Point(137, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 29);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Running...";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(126, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 29);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Not Running";
             // 
             // BreadboardMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Controls.Add(this.MainLabel);
+            this.ClientSize = new System.Drawing.Size(448, 150);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "BreadboardMainForm";
-            this.Text = "Form1";
+            this.Text = "Breadboard";
+            this.Load += new System.EventHandler(this.BreadboardMainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,7 +212,19 @@ namespace Breadboard
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Button rlgFolderButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Breadboard/BreadboardMainForm.cs
+++ Breadboard/BreadboardMainForm.cs
@@ -7,23 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Permissions;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using DataStructures;
 
 
 namespace Breadboard
 {
+
+
     public partial class BreadboardMainForm : Form
     {
+        public static Dictionary<string, string> BreadboardSettings = new Dictionary<string, string>();
+
         public BreadboardMainForm()
         {
             InitializeComponent();
+
+            string settingsFileName = "./BreadboardSettings.set";
+            
+            if (File.Exists(settingsFileName))
+            {
+                BreadboardSettings = ReadDictionary(settingsFileName);
+                if (BreadboardSettings.ContainsKey("SnippetFolder"))
+                {
+                    textBox1.Text = BreadboardSettings["SnippetFolder"];
+                }
+                if (BreadboardSettings.ContainsKey("RunLogFolder"))
+                {
+                    textBox2.Text = BreadboardSettings["RunLogFolder"];
+                }
+                //MessageBox.Show("Settings were loaded from the file 'BreadboardSettings.set' in the directory with the Breadboard executable.");
+            }
+            else
+            {
+                MessageBox.Show("Settings were not automatically loaded. Save new settings to enable automatic loading of settings.");
+            }
+
+
+
+
         }
 
+        // Define the event handlers.
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Debug.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            RunLog rlg = Common.loadBinaryObjectFromFile(e.FullPath) as RunLog;
+            if (BreadboardSettings.ContainsKey("SnippetFolder"))
+            {
+                string destination = BreadboardSettings["SnippetFolder"];
+                writeVariablesToFile(rlg, destination);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a Snippet Folder");
+            }
+        }
 
+        // Manually read runlogs
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
-            Console.WriteLine("S");
+            Debug.WriteLine("S");
             fileDialog.Title = "Open RunLog File(s)";
             fileDialog.Filter = "RunLog files (*.clg)|*.clg|All files (*.*)|*.*";
             fileDialog.FilterIndex = 1;
@@ -40,8 +90,8 @@ namespace Breadboard
             }
         }
 
-
-        public void loadAndWrite(string[] fileNames, string destination)
+        // load files from filenames, and write snippets to destination
+        private void loadAndWrite(string[] fileNames, string destination)
         /// Load Runlog files
         {
             foreach (string fileName in fileNames)
@@ -49,7 +99,7 @@ namespace Breadboard
                 try
                 {
                     RunLog rlg = Common.loadBinaryObjectFromFile(fileName) as RunLog;
-                    Console.WriteLine(rlg.RunTime);
+                    Debug.WriteLine(rlg.RunTime);
                     writeVariablesToFile(rlg, destination);
                 }
                 catch (Exception ex)
@@ -59,12 +109,14 @@ namespace Breadboard
             }
         }
 
-        public bool writeVariablesToFile(RunLog rlg, string path)
+        // Write Runlog to file
+        private bool writeVariablesToFile(RunLog rlg, string path)
         /// write Variable List to File
         {
 
             DateTime runEndTime = rlg.RunTime.AddSeconds(rlg.RunSequence.SequenceDuration);
             string filename = runEndTime.ToString("yyyy-MM-dd");
+            Debug.WriteLine(filename);
             string varFullFileName = path + "\\" + filename + ".txt";
 
             if (path == "")
@@ -74,11 +126,12 @@ namespace Breadboard
 
             try
             {
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(varFullFileName))
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(varFullFileName, append:true))
                 {
                     file.Write(runEndTime.ToString("MM-dd-yyyy_HH_mm_ss\t"));
                     file.Write("SequenceDuration" + ";"+ rlg.RunSequence.SequenceDuration + ",");
-                    file.Write("SequenceModeName" + ";" + rlg.RunSequence.CurrentMode.ModeName + ",");
+                    try { file.Write("SequenceModeName" + ";" + rlg.RunSequence.CurrentMode.ModeName + ","); }
+                    catch { }
                     foreach (Variable var in rlg.RunSequence.Variables)
                     {
                         file.Write(var.VariableName + ';' + Convert.ToString(var.VariableValue) + ",");
@@ -91,10 +144,128 @@ namespace Breadboard
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Unable to open or read file " + filename+" due to exception: " + ex.Message + ex.StackTrace);
+                Debug.WriteLine("Unable to open or read file " + filename+" due to exception: " + ex.Message + ex.StackTrace);
                 return false;
             }
 
+
+        }
+
+        private void BreadboardMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rlgfolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+                BreadboardSettings["RunLogFolder"] = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void snippetfolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                BreadboardSettings["SnippetFolder"] = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            BreadboardSettings["RunLogFolder"] = textBox2.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            BreadboardSettings["SnippetFolder"] = textBox1.Text;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string fileName = "./BreadboardSettings.set";
+            WriteDictionary(BreadboardSettings, fileName);
+
+            MessageBox.Show("Settings saved as 'BreadboardSettings.set'.");
+        }
+
+        static void WriteDictionary(Dictionary<string, string> dictionary, string file)
+        {
+            using (FileStream fs = File.OpenWrite(file))
+            using (BinaryWriter writer = new BinaryWriter(fs))
+            {
+                // Put count.
+                writer.Write(dictionary.Count);
+                // Write pairs.
+                foreach (var pair in dictionary)
+                {
+                    writer.Write(pair.Key);
+                    writer.Write(pair.Value);
+                }
+            }
+        }
+
+        static Dictionary<string, string> ReadDictionary(string file)
+        {
+            var result = new Dictionary<string, string>();
+            using (FileStream fs = File.OpenRead(file))
+            using (BinaryReader reader = new BinaryReader(fs))
+            {
+                // Get count.
+                int count = reader.ReadInt32();
+                // Read in all pairs.
+                for (int i = 0; i < count; i++)
+                {
+                    string key = reader.ReadString();
+                    string value = reader.ReadString();
+                    result[key] = value;
+                }
+            }
+            return result;
+        }
+
+        // START button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (BreadboardSettings.ContainsKey("RunLogFolder") && BreadboardSettings.ContainsKey("SnippetFolder"))
+            {
+                label5.Visible = false;
+                label4.Visible = true;
+                // Create a new FileSystemWatcher and set its properties.
+                FileSystemWatcher watcher = new FileSystemWatcher();
+                watcher.Path = BreadboardSettings["RunLogFolder"];
+                if (BreadboardSettings.ContainsKey("RunLogFolder"))
+                {
+                    watcher.Path = BreadboardSettings["RunLogFolder"];
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a Runlog Folder");
+                }
+                /* Watch for changes in LastAccess and LastWrite times, and
+                   the renaming of files or directories. */
+                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                // Only watch Runlog files.
+                watcher.Filter = "*.clg";
+
+                // Add event handlers.
+                watcher.Created += new FileSystemEventHandler(OnCreated);
+
+                // Begin watching.
+                watcher.EnableRaisingEvents = true;
+            }
+            else
+            {
+                MessageBox.Show("Check your folders!");
+            }
+            
 
         }
     }
